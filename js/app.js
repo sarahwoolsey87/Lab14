fe4f3e49190c7a87e0a25f92651937f6651d97e5
sarahwoolsey87 <shedontusejelly0@gmail.com>
2023-06-29 16:46:24 +0100

    Lab 14

--- js/app.js
+++ js/app.js
@@ -5,31 +5,33 @@ function AppState() {
 }
 
 AppState.prototype.instantiateProducts = function () {
-
   const productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];
 
   for (let i = 0; i < productNames.length; i++) {
     if (productNames[i] === 'sweep') {
-      this.allProducts.push(new Product(productNames[i], 'png'))
+      this.allProducts.push(new Product(productNames[i], 'png'));
     } else {
-      this.allProducts.push(new Product(productNames[i]))
+      this.allProducts.push(new Product(productNames[i]));
     }
   }
-
-}
+};
 
 AppState.prototype.saveToLocalStorage = function () {
-  // TODO: Fill in this instance method to save product data to local storage
-}
+  // Save product data to local storage
+  const productData = JSON.stringify(this.allProducts);
+  localStorage.setItem('productData', productData);
+};
 
 AppState.prototype.loadItems = function () {
-
-  // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
-
-  this.instantiateProducts();
-
-}
-
+  // Retrieve data from local storage instead of creating new Products on each page load
+  const savedProductData = localStorage.getItem('productData');
+  
+  if (savedProductData) {
+    this.allProducts = JSON.parse(savedProductData);
+  } else {
+    this.instantiateProducts();
+  }
+};
 
 function Product(name, fileExtension = 'jpg') {
   this.name = name;
--- js/chart.js
+++ js/chart.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let canvasElem = document.getElementById('chart')
+let canvasElem = document.getElementById('chart');
 
 /* TODO:
  * - Instantiate a new AppState
@@ -10,7 +10,43 @@ let canvasElem = document.getElementById('chart')
  * - Call chart.js with the configuration and the canvasElem
  *
  */
+
 function renderChart() {
+  // Instantiate a new AppState
+  const appState = new AppState();
+
+  // Use a method on that AppState to load vote data from localStorage.
+  appState.loadItems();
+
+  // Create a data object for chart.js using your AppState's allProducts array.
+  const chartData = {
+    labels: appState.allProducts.map(product => product.name),
+    datasets: [
+      {
+        label: 'Vote Count',
+        data: appState.allProducts.map(product => product.timesClicked),
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  // Combine the data object with configuration information for chart.js type, colors, etc
+  const chartConfig = {
+    type: 'bar',
+    data: chartData,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+
+  // Call chart.js with the configuration and the canvasElem
+  new Chart(canvasElem, chartConfig);
 }
 
 renderChart();
